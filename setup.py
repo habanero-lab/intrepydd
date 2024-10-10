d22f804e3624c0fb3cb3929bba6dc470df6edab2
Tong Zhou <tong@192.168.0.196>
2024-10-10 20:44:43 +0800

    made a version that can be uploaded to PyPI

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="intrepydd",                  # Name of the package
+    version="0.1.3",                    # Version
+    author="Tong Zhou",                 # Your name
+    author_email="zt9465@gmail.com", # Your email
+    description="An implementation of the Intrepydd compiler", # Short description
+    long_description=open("README.md").read(), # Long description from README file
+    long_description_content_type="text/markdown", # Type of the long description
+    url="https://github.com/habanero-lab/Onward-Intrepydd", # URL of your project repository
+    packages=find_packages(),           # Automatically find the packages in your project
+    classifiers=[
+        "Programming Language :: Python :: 3",  # Classifiers help users find your project
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',            # Minimum Python version requirement
+    install_requires=["typed_ast", "cppimport", "pytest"]
+)
